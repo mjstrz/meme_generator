cd4b217f868a92f5d003e4304564f3102470c837
mjstrz <strzelecki.monica7@gmail.com>
2022-01-13 22:31:08 -0500

    getMemeImage function added with onclick event handler to button. 1.13.2022 22:31

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react"
 import Header from './components/Header';
 import Meme from "./components/Meme";
 
-/**
- * Challenge: Build the Header component
- */
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Meme />
         </div>
     )    
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import memesData from './components/memesData';
 
 export default function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return(
         <main>
             <form className='form'>
                 <input 
-                    type="text"
-                    placeholder="Top text"
+                    type='text'
+                    placeholder='Top text'
                     className='form--input'
                  />
 
                 <input 
                     type="text"
-                    placeholder="bottom text"
+                    placeholder='bottom text'
                     className='form--input' 
                 />
 
-                <button>Button text</button>
+                <button
+                className='form--button'
+                onClick={getMemeImage}
+                > 
+                Get a new meme image
+                </button>
             </form>
         </main>
     )
