49f63d69146fa6637c4eaa19fd39b1279887e3f4
mjstrz <strzelecki.monica7@gmail.com>
2022-03-21 21:39:22 -0400

    Refactoring state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import memesInfo from './components/memesInfo';
+import memesData from './memesData';
 
 export default function Meme() {
 
-    const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
-        setMemeImage(memesArray[randomNumber].url)
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
 
     return(
@@ -34,7 +43,7 @@ export default function Meme() {
                 Get a new meme image
                 </button>
             </form>
-            <img src = {memeImage} className="meme--image" />
+            <img src = {meme.randomImage} className="meme--image" />
         </main>
     )
 }
